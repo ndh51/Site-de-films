b73d8de7dbe22cce2b13b4e6ee7ea42a44fb8c68
Steven Morlet <steven.morlet@etudiant.univ-reims.fr>
2023-06-12 16:49:11 +0200

    Ajout classes Movie, Genre

---
+++ Entity/Genre.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity;
+
+class Genre
+{
+
+    /* --------------------------------------------------- */
+    /*                      Attributes                     */
+    /* --------------------------------------------------- */
+
+    private int $id;
+    private string $name;
+
+
+
+    /* --------------------------------------------------- */
+    /*                   Getters/Setters                   */
+    /* --------------------------------------------------- */
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+}
