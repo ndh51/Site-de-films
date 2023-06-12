db9303d514f234a8b48c84a5310e5232af4c362f
hale0010 <nadhir.hales@etudiant.univ-reims.fr>
2023-06-12 15:12:30 +0200

    installation finie

---
+++ bin/run-server.bat
@@ -0,0 +1,3 @@
+set APP_DIR=%cd%
+
+php -d display_errors -d auto_prepend_file=%cd%\vendor\autoload.php -S localhost:8000 -t public/
---
+++ bin/run-server.sh
@@ -0,0 +1,1 @@
+APP_DIR="$PWD" php -d display_errors -S localhost:8000 -t public/ -d auto_prepend_file="$PWD/vendor/autoload.php"
