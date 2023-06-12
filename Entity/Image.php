94a46dd557d970f1b8afe871c9202c996b53c278
hale0010 <nadhir.hales@etudiant.univ-reims.fr>
2023-06-12 16:53:16 +0200

    creation des entités

---
+++ Entity/Image.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity;
+
+class image
+{
+    private int $id;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+}
