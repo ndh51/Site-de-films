9c90fd7436772518f676222c1863c1d247a3fdd6
hale0010 <nadhir.hales@etudiant.univ-reims.fr>
2023-06-13 17:11:53 +0200

    ajout du style et finalisation de index.php

--- public/index.php
+++ public/index.php
@@ -12,6 +12,8 @@ $ans=MovieCollection::findAllMovies();
 
 $wp->setTitle('Films');
 
+$wp->appendCssUrl('/index.css');
+
 $wp->appendContent('<header> <h1>Films</h1> </header> ');
 
 $wp->appendContent('<div class="list"> ');
@@ -19,23 +21,24 @@ $wp->appendContent('<div class="list"> ');
 $query=MyPdo::getInstance()->prepare('select jpeg from cutron01_movie.image where id=?');
 
 foreach ($ans as $line) {
-    $wp->appendContent('<div class="movie">');
+
     $wp->appendContent(
         <<<HTML
-    <a href="Movie.php?movieId={$line->getId()}">
+    <div class="movie">
+        <a href="Movie.php?movieId={$line->getId()}">
         <div class="movie_cover">
             <a href="Movie.php?movieId={$line->getId()}"> 
                 <img src='image.php?imageId={$line->getPosterId()}'>
             </a>
         </div>
         <div class="movie_title">
-        <a href="Movie.php?movieId={$line->getId()}">
-            <p> {$line->getTitle()} </p>
-        </a>
+            <a href="Movie.php?movieId={$line->getId()}">
+                <p> {$line->getTitle()} </p>
+            </a>
         </div>
+    </div>
     HTML
     );
-    $wp->appendContent('</div>');
 }
 
 
@@ -44,7 +47,7 @@ $wp->appendContent('</div> ');
 
 
 
-$wp->appendContent('<footer> <h1>Derniere modification :  </h1> </footer> ');
+$wp->appendContent('<footer> <h1> {$ans->getLastModification()} </h1> </footer> ');
 
 
 echo $wp->toHTML();
