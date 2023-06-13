3c63ea9a8776782a1a6c17358f37df0367e3669a
Steven Morlet <steven.morlet@etudiant.univ-reims.fr>
2023-06-13 15:30:35 +0200

    Ajout image.php

--- public/image.php
+++ public/image.php
@@ -1,15 +1,16 @@
 <?php
-
 declare(strict_types=1);
 
-if (! isset($_GET['imageId']) || ! ctype_digit($_GET['imageId'])) {
+use Entity\Image;
+
+if (!isset($_GET['imageId']) || !ctype_digit($_GET['imageId'])) {
     http_response_code(400);
     exit;
 }
 
 $imageId = preg_replace('@<(.+)[^>]*>.*?@is', '', $_GET['imageId']);
 
-$image = Image::findById($imageId);
+$image = Image::findById((int) $imageId);
 
 header('Content-Type: image/jpeg');
 echo $image->getJpeg();
