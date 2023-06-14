96877eb71628e4f7fcd7d211387c9a72c5b69382
Steven Morlet <steven.morlet@etudiant.univ-reims.fr>
2023-06-14 02:42:19 +0200

    Ajout bouton menu pour ajouter film

--- public/index.php
+++ public/index.php
@@ -14,9 +14,16 @@ $wp->setTitle('Films');
 
 $wp->appendCssUrl('/index.css');
 
-$wp->appendContent('<header> <h1>Films</h1> </header> ');
+$wp->appendContent(<<<HTML
+    <header> <h1>Films</h1> </header>
+        <form action="addMovie.php" class="menu_addMovie">
+            <button class="menu_addMovie_button">
+			    <p>Ajouter Film</p>
+			</button>
+        </form>
+        <div class="list">
 
-$wp->appendContent('<div class="list"> ');
+HTML);
 
 $query=MyPdo::getInstance()->prepare('select jpeg from cutron01_movie.image where id=?');
 
@@ -24,25 +31,28 @@ foreach ($ans as $line) {
 
     $wp->appendContent(
         <<<HTML
-    <div class="movie">
-        <a href="movie.php?movieId={$line->getId()}">
-        <div class="movie_cover">
-            <a href="movie.php?movieId={$line->getId()}"> 
-                <img src='poster.php?posterId={$line->getPosterId()}' alt="{$line->getTitle()}">
-            </a>
-        </div>
-        <div class="movie_title">
-            <a href="movie.php?movieId={$line->getId()}">
-                <p> {$line->getTitle()} </p>
-            </a>
-        </div>
-    </div>
+                <div class="movie">
+                    <a href="movie.php?movieId={$line->getId()}">
+                        <div class="movie_cover">
+                            <a href="movie.php?movieId={$line->getId()}"> 
+                                <img src='poster.php?posterId={$line->getPosterId()}' alt="{$line->getTitle()}">
+                            </a>
+                        </div>
+                        <div class="movie_title">
+                            <a href="movie.php?movieId={$line->getId()}">
+                                <p> {$line->getTitle()} </p>
+                            </a>
+                        </div>
+                    </a>
+                </div>
+
     HTML
     );
 }
 
-$wp->appendContent('</div> ');
+$wp->appendContent('        </div> ');
 
-$wp->appendContent("<footer> <h2> {$wp->getLastModification()} </h2> </footer> ");
+$wp->appendContent("
+        <footer> <h2> {$wp->getLastModification()} </h2> </footer> ");
 
 echo $wp->toHTML();
