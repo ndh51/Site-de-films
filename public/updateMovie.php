a9e21cd35832bfbaa0b1f38d5741a8047a15154c
Steven Morlet <steven.morlet@etudiant.univ-reims.fr>
2023-06-14 14:29:12 +0200

    Ajout scripts pour menus : non écrits et non fonctionnels

---
+++ public/updateMovie.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+use Entity\Exception\EntityNotFoundException;
+use Entity\Exception\ParameterException;
+use Entity\Movie;
+use Html\WebPage;
+
+$updateMoviePage = new WebPage();
+/*
+try {
+    if (!isset($_GET['movieId']) || !ctype_digit($_GET['movieId']) || $_GET['movieId'] < 0) {
+        throw new ParameterException();
+    }
+    $movieId = preg_replace('@<(.+)[^>]*>.*?@is', '', $_GET['movieId']);
+    if (! Movie::findById((int) $movieId)) {
+        throw new EntityNotFoundException();
+    }
+} catch (ParameterException) {
+    http_response_code(400);
+    exit;
+} catch (EntityNotFoundException) {
+    http_response_code(404);
+    exit;
+} catch (Exception) {
+    http_response_code(500);
+    exit;
+}
+ */
+
+$movieId = preg_replace('@<(.+)[^>]*>.*?@is', '', $_GET['movieId']);
+
+$movie = Movie::findById((int)$movieId);
+
+$updateMoviePage -> setTitle("Modification de {$movie->getTitle()}");
+
+$updateMoviePage -> appendContent("<h1>test</h1>");
