92114b3214d889415780167070265e9a8dd90ae3
Steven Morlet <steven.morlet@etudiant.univ-reims.fr>
2023-06-14 20:58:25 +0200

    Ajout exceptions confirmUpdateMovie

---
+++ public/confirmEditData/confirmUpdateMovie.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+use Database\MyPdo;
+use Entity\Exception\ParameterException;
+use Html\WebPage;
+
+
+$confirmEditMoviePage = new WebPage("Page de confirmation : Film");
+
+/** Fait une vérification du paramètre donné
+ *
+ * @param string $param
+ * @return string
+ */
+function verify_exception(string $param): string
+{
+    try {
+        if (!isset($_POST[$param])) {
+            throw new ParameterException();
+        }
+        $paramSecured = preg_replace('@<(.+)[^>]*>.*?@is', '', $_POST[$param]);
+    } catch (ParameterException) {
+        http_response_code(400);
+        exit;
+    } catch (Exception) {
+        http_response_code(500);
+        exit;
+    }
+    return $paramSecured;
+}
+
+$id = verify_exception("id");
+$title = verify_exception("title");
+$releaseDate = verify_exception("releaseDate");
+$originalTitle = verify_exception("originalTitle");
+$tagline = verify_exception("tagline");
+$overview = verify_exception("overview");
+
+try {
+    if (!ctype_digit($id) || $id < 0 || $id == '') {
+        throw new ParameterException();
+    }
+} catch (ParameterException) {
+    header('Location: ../index.php');
+    http_response_code(400);
+    exit;
+}
