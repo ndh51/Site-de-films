2cd36bb66da522059a3d142b4c771a67c0cb15e3
Steven Morlet <steven.morlet@etudiant.univ-reims.fr>
2023-06-14 18:04:14 +0200

    Ajout confirmEditMovie verif exceptions + recuperation parametres

---
+++ public/confirmEditData/confirmEditMovie.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+use Database\MyPdo;
+use Entity\Exception\ParameterException;
+use Html\WebPage;
+
+
+$confirmEditMoviePage = new WebPage("Page de confirmation : Film");
+
+/** Fait une vérification du paramètre donné
+ *
+ * @param string $param
+ * @return string
+ */
+function verify_exception(string $param): string
+{
+    try {
+        if (!isset($_GET[$param])) {
+            throw new ParameterException();
+        }
+        $paramSecured = preg_replace('@<(.+)[^>]*>.*?@is', '', $_GET[$param]);
+    } catch (ParameterException) {
+        http_response_code(400);
+        exit;
+    } catch (Exception) {
+        http_response_code(500);
+        exit;
+    }
+    return $paramSecured;
+}
+
+$id = verify_exception("id");
+$title = verify_exception("title");
+$releaseDate = verify_exception("releaseDate");
+$originalTitle = verify_exception("originalTitle");
+$tagline = verify_exception("tagline");
+$overview = verify_exception("overview");
+
+try {
+    if ($_GET['id'] == '') {
+        throw new ParameterException();
+    }
+    foreach ($ids as $movieid) {
+        if ($id == $movieid) {
+            throw new ParameterException();
+        }
+    }
+} catch (ParameterException) {
+    header('Location: ../menus/addMovie.php');
+    http_response_code(400);
+    exit;
+}
